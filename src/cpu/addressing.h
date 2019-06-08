876500ee7fa8b14a87fc4e26336a45172fbe19dc
Ryan Daum <ryan.daum@gmail.com>
2019-06-08 00:42:44 -0400

    Add literals for addressing modes, for logging purposes.

--- src/cpu/addressing.h
+++ src/cpu/addressing.h
@@ -52,4 +52,34 @@ enum class AddressingMode {
   ProgramCounterRelativeLong
 };
 
+constexpr const char *kAddressingModeNames[]{
+    "Interrupt",
+    "Accumulator",
+    "BlockMove",
+    "Implied",
+    "Immediate",
+    "Absolute",
+    "AbsoluteLong",
+    "AbsoluteIndirect",
+    "AbsoluteIndirectLong",
+    "AbsoluteIndexedIndirectWithX", // index with X
+    "AbsoluteIndexedWithX",
+    "AbsoluteLongIndexedWithX",
+    "AbsoluteIndexedWithY",
+    "DirectPage",
+    "DirectPageIndexedWithX",
+    "DirectPageIndexedWithY",
+    "DirectPageIndirect",
+    "DirectPageIndirectLong",
+    "DirectPageIndexedIndirectWithX",
+    "DirectPageIndirectIndexedWithY",
+    "DirectPageIndirectLongIndexedWithY",
+    "StackImplied",
+    "StackRelative",
+    "StackAbsolute",
+    "StackDirectPageIndirect",
+    "StackProgramCounterRelativeLong",
+    "StackRelativeIndirectIndexedWithY",
+    "ProgramCounterRelative",
+    "ProgramCounterRelativeLong"};
 #endif  // ADDRESSING_HPP
